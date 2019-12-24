6d259f8f6c1566a3f70b601b23b5dc469478ba5a
alexmorris10x <alexmorris10x@icloud.com>
2019-12-23 18:51:52 -0800

    .

--- flask_qa/commands.py
+++ flask_qa/commands.py
@@ -10,8 +10,8 @@ from .models import Stocks
 @with_appcontext
 def create_tables():
     db.create_all()
-    # newStocks = Stocks(
-    #     name='username', share='Apple, Inc.', shares=1, price=284, time='2019-12-23 21:17:42.938156', cash=9716
-    # )
-    # db.session.add(newStocks)
-    # db.session.commit()
+    newStocks = Stocks(
+        name='username', share='Apple, Inc.', shares=1, price=284, time='2019-12-23 21:17:42.938156', cash=9716
+    )
+    db.session.add(newStocks)
+    db.session.commit()
